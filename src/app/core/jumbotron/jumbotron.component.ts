6c807f8daff5b130827b6752ac40973bf9d465e6
Gianmarco Culqui <gianmarcoculqui@MBPdiGianmarco.station>
2023-06-28 22:47:37 +0200

    first commit

--- src/app/core/jumbotron/jumbotron.component.ts
+++ src/app/core/jumbotron/jumbotron.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // @ts-ignore
-import {Typewriter} from 't-writer.js';
+
 
 @Component({
   selector: 'app-jumbotron',
@@ -10,31 +10,6 @@ import {Typewriter} from 't-writer.js';
 export class JumbotronComponent implements OnInit  {
   constructor(){}
   ngOnInit(): void {
-    const target = document.querySelector('.tw');
-
-    const writer = new Typewriter(target, {
-      loop: true,
-      typeSpeed: 80,
-      deleteSpeed: 80,
-      typeColor: '#ffe22d'
-    })
-
-    writer
-      .type('di gestire il tuo immobile')
-      .rest(500)
-      .changeOps({ deleteSpeed: 50})
-      .remove(26)
-      .type('dei mancati pagamenti')
-      .rest(500)
-      .remove(34)
-      .type('della manutenzione')
-      .rest(500)
-      .changeOps({ deleteSpeed: 20 })
-      .remove(21)
-
-      .clear()
-      .start()
-
 
   }
 
